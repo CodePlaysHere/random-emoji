bb3854d869fdb47b4fe32ce7e1c094f9be21efce
Code Geek <codeplayshere@gmail.com>
2025-02-24 23:06:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -40,3 +40,8 @@ const emojis = [
 btn.addEventListener("mouseover", () => {
 	btn.innerText = emojis[Math.floor(Math.random() * emojis.length)];
 });
+
+
+btn.addEventListener("click", () => {
+	btn.innerText = emojis[Math.floor(Math.random() * emojis.length)];
+});
